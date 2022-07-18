e1d5d55f74963d43f717fbdd554616755d3e5b43
stevenmalley <steven.malley@gmail.com>
2022-07-18 14:47:37 +0100

    package-lock files git ignored

--- client/src/fetcher.js
+++
@@ -1,8 +0,0 @@
-export default async function fetcher(path,options) {
-  path = "/api"+path;
-
-  if (document.location.port === "3000")
-    path = "http://localhost:8080"+path;
-  
-  return await fetch(path,options);
-}
