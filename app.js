c503eb2c03139547d64faea79d77e1ebb46665bd
stevenmalley <steven.malley@gmail.com>
2022-07-24 19:00:56 +0100

    environment variable renamed

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const Pool = require('pg').Pool
-const pool = new Pool({connectionString: process.env.DB_CONNECTION_STRING});
+const pool = new Pool({connectionString: process.env.DATABASE_URL});
 const db = require('./db')(pool);
 
 const bodyParser = require("body-parser");
