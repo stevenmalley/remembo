ba988c848ef0aa4f0a8f47534cb953eaff0eb258
stevenmalley <steven.malley@gmail.com>
2024-09-25 12:38:22 +0100

    fix Heroku routing error

--- app.js
+++ app.js
@@ -84,9 +84,8 @@ app.use(express.static("client/build"));
 if (process.env.DEVELOPMENT !== "true") {
   app.get(/^((?!api).)*$/, // any path except those with "api", redirect to index.html for React Router to handle
     (req, res) => {
-      console.log("redirecting to remembo.herokuapp.com");
-      //res.sendFile(path.resolve('index.html'));
-      res.sendFile('https://remembo.herokuapp.com');
+      console.log("redirecting to "+path.join(__dirname, 'build/index.html'));
+      res.sendFile(path.join(__dirname, 'build/index.html'));
     }
   );
 }
